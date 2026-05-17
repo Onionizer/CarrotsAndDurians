065405574947b8d5c6b9b7b39881d8e68ae1d065
Onionizer <thethunderthigh@gmail.com>
2026-05-17 01:21:00 -0700

    Trying Mapbox

--- project_999/static/js/Navbar.js
+++ project_999/static/js/Navbar.js
@@ -4,10 +4,10 @@ function Navbar() {
     return (
         <nav className="Navbar" id="navbar">
             <h1>Carpool Connector</h1>
-            <button tabindex="0">Home</button>
-            <button tabindex="0">Search Rides</button>
-            <button tabindex="0">Register route</button>
-            <button tabindex="0">Profile</button>
+            <button tabIndex="0">Home</button>
+            <button tabIndex="0">Search Rides</button>
+            <button tabIndex="0">Register route</button>
+            <button tabIndex="0">Profile</button>
         </nav>
     );
 }
