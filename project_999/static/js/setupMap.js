e4eb88aeab9e11a6178c644394de206654c51522
Onionizer <thethunderthigh@gmail.com>
2026-05-17 03:23:26 -0700

    asdf

---
+++ project_999/static/js/setupMap.js
@@ -0,0 +1,32 @@
+console.log("Setting up map . . .");
+
+// Copied from project 2
+const map = L.map('map').setView([ 37.87, -122.27 ], 14);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// Break these down into smaller boxes and concat them. For now, just one box for prototyping
+function getHomeAddress() {
+    return document.getElementById("home-address").value;
+}
+
+function getWorkAddress() {
+    return document.getElementById("work-address").value;
+}
+
+
+async function calculateAndDisplayRoute() {
+    //  prob easier to just have one box for now 
+    const homeAddress = getHomeAddress();
+    const workAddress = getWorkAddress();
+
+    try {
+
+    } catch (error) {
+        console.error("Error calculating/displaying route:", error);
+    }
+}
