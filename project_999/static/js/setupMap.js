dde11caa11fe4ba0e66c4f050a7b8c5465187d44
Onionizer <thethunderthigh@gmail.com>
2026-05-17 09:42:52 -0700

    Test with UC Berkeley and downtown Reedwood City

--- project_999/static/js/setupMap.js
+++ project_999/static/js/setupMap.js
@@ -38,8 +38,26 @@ async function textAddressToLatLong(address) {
         console.error("Error converting address to lat/long:", error);
     }
 }
-textAddressToLatLong("UC Berkeley, Berkeley, CA");
-// textAddressToLatLong("Downtown Redwood City, CA");
+// textAddressToLatLong("UC Berkeley, Berkeley, CA");
+// [
+//   {
+//     "place_id": 319946266,
+//     "licence": "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright",
+//     "osm_type": "node",
+//     "osm_id": 1948043265,
+//     "boundingbox": ["37.8713458", "37.8714458", "-122.2651894", "-122.2650894"],
+//     "lat": "37.8713958",
+//     "lon": "-122.2651394",
+//     "display_name": "UC Berkeley, The Crescent, Downtown Berkeley, Berkeley, Alameda County, California, 94704, United States",
+//     "class": "highway",
+//     "type": "bus_stop",
+//     "addresstype": "highway",
+//     "name": "UC Berkeley",
+//     "importance": 0.00007045166704809671,
+//     "place_rank": 30
+//   }
+// ]
+textAddressToLatLong("Downtown Redwood City, CA");
 // textAddressToLatLong("Walnut Creek, CA");
 
 // Break these down into smaller boxes and concat them. For now, just one box for prototyping
@@ -55,8 +73,13 @@ function getWorkAddress() {
 let currentRoutingControl = null;
 async function calculateAndDisplayRoute() {
     //  prob easier to just have one box for now 
-    const homeAddress = getHomeAddress();
-    const workAddress = getWorkAddress();
+    // const homeAddress = getHomeAddress();
+    const homeAddress = textAddressToLatLong("UC Berkeley, CA");
+    console.log("Home address lat/long:", homeAddress);
+    
+    // const workAddress = getWorkAddress();
+    const workAddress = textAddressToLatLong("Downtown Redwood City, CA");
+    console.log("Work address lat/long:", workAddress);
 
     try {
         // Create URL for OpenRouteService API call
