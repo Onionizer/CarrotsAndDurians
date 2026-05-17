ac7ac32c02ea525d1974a69244a74b07e010490a
Onionizer <thethunderthigh@gmail.com>
2026-05-17 03:46:24 -0700

    Save

--- project_999/static/js/setupMap.js
+++ project_999/static/js/setupMap.js
@@ -1,12 +1,16 @@
 console.log("Setting up map . . .");
 
 // Copied from project 2
-const map = L.map('map').setView([ 37.87, -122.27 ], 14);
+// Higher = zoom in closer.  
+const map = L.map('map').setView([ 37.87, -122.27 ], 10);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// Have a list of pins for pickup spots
 
 
 // Break these down into smaller boxes and concat them. For now, just one box for prototyping
@@ -18,7 +22,8 @@ function getWorkAddress() {
     return document.getElementById("work-address").value;
 }
 
-
+// Need a way to erase old routes when displaying new one
+let currentRoutingControl = null;
 async function calculateAndDisplayRoute() {
     //  prob easier to just have one box for now 
     const homeAddress = getHomeAddress();
