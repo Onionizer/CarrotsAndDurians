ff0b32dce11741b0afa701dc0c1c61bd6568cf3a
Onionizer <thethunderthigh@gmail.com>
2026-05-18 16:22:01 -0700

    Try getting data from Firebase

--- project_999/static/js/loadRoutes.js
+++ project_999/static/js/loadRoutes.js
@@ -5,6 +5,14 @@ function fetchRoutesFromFirebase() {
     
     // For each one, create a row.  
     // Need to create a mini map for route.  Just assume center is the same for all?
-    db.collection(FIREBASE_PROTOTYPE_NO_AUTH_COLLECTION).get();
-    
+    db.collection(FIREBASE_PROTOTYPE_NO_AUTH_COLLECTION).get()
+        .then((querySnapshot) => {
+            console.log("Testing Firestore Connection...");
+            querySnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+            });
+        })
+        .catch(err => console.error("Firebase fetch failed:", err));
 }
+
+fetchRoutesFromFirebase();
