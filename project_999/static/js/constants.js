96c278d2dc123b668715820279713a5021bdd95b
Onionizer <thethunderthigh@gmail.com>
2026-05-17 23:20:46 -0700

    Add constants.js

---
+++ project_999/static/js/constants.js
@@ -0,0 +1,14 @@
+// Firebase Config
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCFf1ll6vhRJ5yF5E1b4jC-k83ecWfi7Nk",
+  authDomain: "carpool-connector-dcef1.firebaseapp.com",
+  projectId: "carpool-connector-dcef1",
+  storageBucket: "carpool-connector-dcef1.firebasestorage.app",
+  messagingSenderId: "914405225086",
+  appId: "1:914405225086:web:b96a02f81aaddb52ab9e2f",
+  measurementId: "G-13MXP1PFWW"
+};
+
+
+
+export const FIREBASE_PROTOTYPE_NO_AUTH_COLLECTION = "commuter_routes_no_auth";  // Name for prototype - no authentication for this one
