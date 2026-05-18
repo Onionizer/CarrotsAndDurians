7ad24d81f0bf8713f60183b8c838bcf287766e43
Onionizer <thethunderthigh@gmail.com>
2026-05-18 15:58:06 -0700

    Increase route line weight from 6 to 9

--- project_999/static/js/constants.js
+++ project_999/static/js/constants.js
@@ -9,12 +9,9 @@ export const FIREBASE_CONFIG = {
   measurementId: "G-13MXP1PFWW"
 };
 
-
-
 export const FIREBASE_PROTOTYPE_NO_AUTH_COLLECTION = "commuter_routes_no_auth";  // Name for prototype - no authentication for this one
 
 // Need to secure this.
 export const OPENROUTESERVICE_API_KEY = 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjM4ZTEwZDU4MzhmNzRmNDNiNmVmMDc3ZDEzZTk0ODY1IiwiaCI6Im11cm11cjY0In0=';
 
-
-export const ROUTE_STYLE = [{ color: '#2A75D3', weight: 6, opacity: 0.85 }] ;
+export const ROUTE_STYLE = [{ color: '#2A75D3', weight: 9, opacity: 0.85 }] ;
