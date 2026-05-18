78b2671d980f01db4f61e939ee5d0735bde734df
Onionizer <thethunderthigh@gmail.com>
2026-05-18 16:11:59 -0700

    Update href to rides.html

--- project_999/static/js/Navbar.js
+++ project_999/static/js/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
             <h1>Carpool Connector</h1>
             <ul>
                 <li><a tabIndex="0" href="index.html">Home</a></li>
-                <li><a tabIndex="0" href="find-rides.html">Find Rides</a></li>
+                <li><a tabIndex="0" href="rides.html">Find Rides</a></li>
                 <li><a tabIndex="0" href="profile.html">Profile / Register Route</a></li>
             </ul>
         </nav>
