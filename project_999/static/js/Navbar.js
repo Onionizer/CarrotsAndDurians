565efbad67cda5f046dd7841c3f58edc4f208ea8
Onionizer <thethunderthigh@gmail.com>
2026-05-18 16:10:03 -0700

    Update href links in navbar

--- project_999/static/js/Navbar.js
+++ project_999/static/js/Navbar.js
@@ -1,14 +1,13 @@
 function Navbar() {
-    console.log("In Navbar() . . .");
+    // console.log("In Navbar() . . .");
 
     return (
         <nav className="Navbar" id="navbar" role="navigation" aria-label="main navigation">
             <h1>Carpool Connector</h1>
             <ul>
-                <li><a tabIndex="0" href="#home">Home</a></li>
-                <li><a tabIndex="0" href="#search-rides">Search Rides</a></li>
-                <li><a tabIndex="0" href="#register-route">Register Route</a></li>
-                <li><a tabIndex="0" href="#profile">Profile</a></li>
+                <li><a tabIndex="0" href="index.html">Home</a></li>
+                <li><a tabIndex="0" href="find-rides.html">Find Rides</a></li>
+                <li><a tabIndex="0" href="profile.html">Profile / Register Route</a></li>
             </ul>
         </nav>
     );
