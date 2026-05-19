cc73610293ef957ea5583bb193095dced7e09425
Onionizer <thethunderthigh@gmail.com>
2026-05-18 17:54:30 -0700

    Debuggin

--- project_999/static/js/setupMap.js
+++ project_999/static/js/setupMap.js
@@ -21,13 +21,6 @@ async function textAddressToLatLong(address) {
     console.log("Converting address to latitude & longitude:", address);
     const URL = `https://nominatim.openstreetmap.org/search?format=json&limit=1&q=${encodeURIComponent(address)}`;
 
-    // return fetch(URL)
-    //     .then((response) => response.json()) 
-    //     .then((data) => {
-    //         console.log("Received loc data:", data);
-    //         return L.latLng(data[0].lat, data[0].lon);
-    //     });
-
     try {
         const response = await fetch(URL);
         const data = await response.json();
@@ -39,44 +32,6 @@ async function textAddressToLatLong(address) {
         console.error("Error converting address to lat/long:", error);
     }
 }
-// textAddressToLatLong("UC Berkeley, Berkeley, CA");
-// [
-//   {
-//     "place_id": 319946266,
-//     "licence": "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright",
-//     "osm_type": "node",
-//     "osm_id": 1948043265,
-//     "boundingbox": ["37.8713458", "37.8714458", "-122.2651894", "-122.2650894"],
-//     "lat": "37.8713958",
-//     "lon": "-122.2651394",
-//     "display_name": "UC Berkeley, The Crescent, Downtown Berkeley, Berkeley, Alameda County, California, 94704, United States",
-//     "class": "highway",
-//     "type": "bus_stop",
-//     "addresstype": "highway",
-//     "name": "UC Berkeley",
-//     "importance": 0.00007045166704809671,
-//     "place_rank": 30
-//   }
-// ]
-// textAddressToLatLong("Downtown Redwood City, CA");
-// [
-//   {
-//     "place_id": 324720148,
-//     "licence": "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright",
-//     "osm_type": "way",
-//     "osm_id": 240211431,
-//     "boundingbox": ["37.4853549", "37.4855876", "-122.2279621", "-122.2276210"],
-//     "lat": "37.4854375",
-//     "lon": "-122.2277882",
-//     "display_name": "Downtown Station Redwood City Post Office, 855, Jefferson Avenue, Redwood Junction, Redwood City, San Mateo County, California, 94063, United States",
-//     "class": "amenity",
-//     "type": "post_office",
-//     "addresstype": "amenity",
-//     "name": "Downtown Station Redwood City Post Office",
-//     "importance": 0.00006208209353775871,
-//     "place_rank": 30
-//   }
-// ]
 
 
 // Break these down into smaller boxes and concat them. For now, just one box for prototyping
@@ -130,18 +85,12 @@ function pinPickUpSpots() {
 pinPickUpSpots();
 
 
-class CommuteRoute {
-    // want user information, max distance added/max time added, but for prototype, just route.
-    constructor(homeCoordinates, workCoordinates, passengerCapacity) {
-        this.homeCoordinates = homeCoordinates;
-        this.workCoordinates = workCoordinates;
-        this.passengerCapacity = passengerCapacity;
-    }
-}
-
-
 firebase.initializeApp(FIREBASE_CONFIG);
 const db = firebase.firestore();
+if (!db) {
+    console.error("db firestore not initialized properly");
+}
+
 
 async function persistRouteToFirebase(homeCoordinates, workCoordinates, passengerCapacity) {
     console.log("Persisting route data to Firebase . . .");
