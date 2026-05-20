185c72d048352b396b1a54ec9ff33daca516773d
Onionizer <thethunderthigh@gmail.com>
2026-05-19 18:29:34 -0700

    Try removing the extra div that's not needed to move home button

--- project_999/static/js/Navbar.js
+++ project_999/static/js/Navbar.js
@@ -1,17 +1,15 @@
 function Navbar() {
     return (
         <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-            <div className="container">
-                <div className="navbar-brand">
-                    <a className="navbar-item has-text-weight-bold" href="index.html">
-                        Carpool Connector
-                    </a>
-                </div>
-                <div className="navbar-menu is-active">
-                    <div className="navbar-start">
-                        <a className="navbar-item" href="rides.html">Search Rides</a>
-                        <a className="navbar-item" href="profile.html">Register Route</a>
-                    </div>
+            <div className="navbar-brand">
+                <a className="navbar-item has-text-weight-bold" href="index.html">
+                    Carpool Connector
+                </a>
+            </div>
+            <div className="navbar-menu is-active">
+                <div className="navbar-start">
+                    <a className="navbar-item" href="rides.html">Search Rides</a>
+                    <a className="navbar-item" href="profile.html">Register Route</a>
                 </div>
             </div>
         </nav>
